4488a24d1f47694cb87c4f0356c16567c88220b8
Mayra B <mayra_vafer@hotmail.com>
2018-03-04 11:14:09 -0600

    Ok

--- Reporte2/rep2.py
+++ Reporte2/rep2.py
@@ -7,7 +7,7 @@ class Grafo:
 		self.x = dict()		#
 		self.y = dict()		#
 		self.P = []	
-		self.dist = []
+		self.pesos = []
 		self.nodo2 = []
 		self.nodo3 = []	
 		self.Ari = []	#
@@ -23,14 +23,14 @@ class Grafo:
 	def aristas(self, prob):
 		for i in range(self.n - 1):
 			self.nodo2.append(self.P[i])
-		for i in range(1, self.n):
+		for i in range(self.n):
 			self.nodo3.append(self.P[i])
 		for(x1, y1, i) in self.nodo2:
 			del self.nodo3[0]
 			for(x2, y2, j) in self.nodo3:
 				if random() < prob:
 					self.Ari.append((x1, y1, x2, y2))
-					
+					self.pesos.append((sqrt((x2 - x1) ** 2 + (y2 - y1 )** 2)*100, (x1+x2)/2, (y1+y2)/2))
 		print(len(self.Ari))
 
 		
@@ -39,35 +39,38 @@ class Grafo:
 	def imprimir(self, arch):
 		self.archivo = arch
 		#for k in range(len(self.Ari)):
-			#self.dist[k] = sqrt(((self.Ari[k][0] - self.Ari[k][1]) ** 2 )+ ((self.Ari[k][2] - self.Ari[k][3]) ** 2))
+			#
 
 		with open (self.archivo, "w") as salida:
 			for nodo in range(self.n):
 				print(self.P[nodo][0], self.P[nodo][1], (random() * 10),  file = salida)
 		print(self.archivo)
 
-	def grafica (self, di, pe):
+	#di = 1 es simple
+	#di = 2 es dirigido
+	#di = 3 es ponderado
+
+	def grafica (self, di):
 		assert self.archivo is not None
 		with open("nodos.plot", 'w') as salida: 
-			print('set term eps', file = salida)
-			print('set output "nodos.eps"', file = salida)
+			print('set term png', file = salida)
+			print('set output "nodos.png"', file = salida)
 			print('set size square', file = salida)
 			print('set key off', file = salida)
 			print ('set xrange [-.1:1.1]', file = salida)
 			print ('set yrange [-.1:1.1]', file = salida)
 			id = 1
 			for i in range(len(self.Ari)):
-				if di is 1 :
+				if di is 2 :
 					print('set arrow', id, 'from', self.Ari[i][0], ',', self.Ari[i][1], 'to', self.Ari[i][2], ',', self.Ari[i][3], 'head filled lw 1',  file = salida)
 					id +=1
-				else:
+				elif di is 1:
 					print('set arrow', id, 'from', self.Ari[i][0], ',', self.Ari[i][1], 'to', self.Ari[i][2], ',', self.Ari[i][3], 'nohead filled lw 1', file = salida)
 					id +=1
-				#if pe is 1:
+				elif di is 3:
+					print('set arrow', id, 'from', self.Ari[i][0], ',', self.Ari[i][1], 'to', self.Ari[i][2], ',', self.Ari[i][3], 'nohead filled lw 1', file = salida)
+					print('set label', "'", int(self.pesos[i][0]), "'", 'at', self.pesos[i][1], ',', self.pesos[i][2], file = salida)
+					id +=1
 
 			print('plot "nodos.dat" using 1:2:3 with points pt 7 lc var ps 2 ', file = salida)
 			print('quit()', file =  salida)
-
-
-
-
