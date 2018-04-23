1fc66f5b4dbbb67250a4ad0981cc4dbd409b66a6
Mayra B <mayra_vafer@hotmail.com>
2018-04-23 01:07:37 -0500

    Modif

--- Reporte4/try.py
+++ Reporte4/try.py
@@ -3,9 +3,9 @@ import time
 nodos = [10, 20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200] 
 p = Grafo()
 
-p.puntos(10, 2)
-p.k_conect(2)
-p.conecta(.1)
+p.puntos(40, 15)
+p.k_conect(15)
+p.conecta(.001)
 p.graficar()
 p.diagrama2()
 
