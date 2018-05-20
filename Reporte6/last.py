27ae18f04422f6c2fcfae9ac0c57461cdc2d3d79
Mayra B <mayra_vafer@hotmail.com>
2018-05-20 18:12:32 -0500

    itn2

--- Reporte6/last.py
+++ Reporte6/last.py
@@ -11,21 +11,18 @@ class Grafo:
 		self.nodos = []
 		self.A = []
 		self.pesos = []
-		self.sec = []
+
 		self.aux = []
 
 	def puntos (self, num):
 		self.n = num
 		self.nodos.append((-.3, 0.5, 0))
 		self.aux.append((0))
-		self.sec.append((0))
 		for i in range(1, self.n - 1):
 			self.nodos.append((random(), random(), i))
 			self.aux.append((i))
-			self.sec.append((i))
 		self.nodos.append((1.3, 0.5, self.n -1))
 		self.aux.append((self.n -1))
-		self.sec.append((self.n -1))
 
 	def conect (self, prob):
 		for (x1, y1, u) in self.nodos:
@@ -42,23 +39,21 @@ class Grafo:
 			if w is not z:
 				if w is not self.nodos[0][2] and w is not self.nodos[len(self.nodos) - 1][2]:
 					if w in self.aux:
-						self.sec[z] = str(self.sec[z]) +"#"+ str(self.sec[w])
 						self.aux.remove(w)
-						
 						print (self.aux)
-						print (z, w)
+						print (w, z)
 						for i in range(0, len(self.nodos) - 1):
 							if w is self.nodos[i][2]:
 								self.nodos.pop(i)
 								print(self.nodos)	
 
 		print(self.nodos)
-		print(self.sec)
 					
 
 
-	def imprimir(self):
-		with open("grafo.dat", 'w') as salida:
+
+	def imprimir(self, num):
+		with open("grafo"+str(num)+".dat", 'w') as salida:
 			for n in range(self.n):
 				print(self.nodos[n][0], self.nodos[n][1], self.nodos[n][2], file = salida)
 
@@ -70,30 +65,7 @@ class Grafo:
 
 
 
-	def grafica (self, eps = True):
-		with open("nodos.plot", 'w') as salida: 
-			if eps:
-				print("set term postscript color eps", file = salida)
-				print('set output "nodos.eps"', file = salida)
-			else:
-				print('set term png', file = salida)
-				print('set output "nodos.png"', file = salida)
-			print('set size square', file = salida)
-			print('set key off', file = salida)
-			print ('set xrange [-.5:1.5]', file = salida)
-			print ('set yrange [-.1:1.1]', file = salida)
-			for n in range(len(self.nodos)):
-				print('set label', "   ' " , int(self.nodos[n][2]), "   ' ", 'at', self.nodos[n][0], ",", self.nodos[n][1],  file = salida )
-			
-			id = 1
-			for i in range(len(self.A)):
-				print('set arrow', id, 'from', self.A[i][0], ',', self.A[i][1], 'to', self.A[i][2], ',', self.A[i][3], 'nohead filled lw 1', file = salida)
-				print('set label', " ' ", int(self.pesos[i][0]), " ' ", 'at', self.pesos[i][1], ',', self.pesos[i][2], file = salida)
-				id += 1
-			print('plot "grafo.dat" using 1:2:3 with points pt 7 lc var ps 2 ', file = salida)
-			print('quit()', file =  salida)
-
-	def grafica2(self,num, eps = True):
+	def grafica (self,num,  eps = True):
 		with open("nodos"+str(num)+".plot", 'w') as salida: 
 			if eps:
 				print("set term postscript color eps", file = salida)
@@ -105,9 +77,8 @@ class Grafo:
 			print('set key off', file = salida)
 			print ('set xrange [-.5:1.5]', file = salida)
 			print ('set yrange [-.1:1.1]', file = salida)
-
-			print('set label', " ' " , self.sec[0], "   ' ", 'at', self.nodos[0][0], ",", self.nodos[0][1],  file = salida)
-			print('set label', " ' " , self.sec[self.n - 1], "   ' ", 'at', self.nodos[1][0], ",", self.nodos[1][1],  file = salida )
+			for n in range(len(self.nodos)):
+				print('set label', " ' " , self.nodos[n][2], "   ' ", 'at', self.nodos[n][0], ",", self.nodos[n][1],  file = salida )
 			
 			id = 1
 			for i in range(len(self.A)):
@@ -117,12 +88,14 @@ class Grafo:
 			print('plot "grafo'+str(num)+'.dat" using 1:2:3 with points pt 7 lc var ps 2 ', file = salida)
 			print('quit()', file =  salida)
 
+
 p = Grafo()
 p.puntos(10)
-p.imprimir()
+p.imprimir(1)
 p.conect(0.2)
-p.grafica()
 p.merge()
+p.grafica(1)
 p.imprimir2(2)
-p.grafica2(2)
+p.grafica(2)
+
 
